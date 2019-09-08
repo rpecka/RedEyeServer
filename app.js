b51ed2b2d3fc9794805d918d796fdb1867ba29e4
Russell Pecka <rpecka98@gmail.com>
2019-09-08 03:05:18 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,53 @@
+// Copyright 2018, Google LLC.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+const express = require('express');
+const textToSpeech = require('@google-cloud/text-to-speech');
+const bodyParser = require('body-parser');
+
+const client = new textToSpeech.TextToSpeechClient();
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// [START hello_world]
+// Say hello!
+app.post('/synthesize', (req, res) => {
+	console.log("Hi");
+	console.log(req.body);
+	var request = {
+		input: {text: req.body.text},
+		voice: {languageCode: 'en-US', name: 'en-US-Wavenet-F'},
+		audioConfig: {audioEncoding: 'MP3'},
+	}
+	client.synthesizeSpeech(request).then(function(response) {
+		console.log(response[0]);
+		res.status(200).send(response[0].audioContent);
+	});
+});
+// [END hello_world]
+
+if (module === require.main) {
+  // [START server]
+  // Start the server
+  const server = app.listen(process.env.PORT || 8080, () => {
+    const port = server.address().port;
+    console.log(`App listening on port ${port}`);
+  });
+  // [END server]
+}
+
+module.exports = app;
